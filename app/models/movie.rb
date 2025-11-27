4e3f30f3f79544e8aff6eacff3e1e64dc5a79d2d
Lalon Sander <lalon.sander@gmail.com>
2025-11-27 11:05:36 +0100

    db seeded

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,16 @@ class Movie < ApplicationRecord
   has_many :bookmarks
 
   validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+
+  before_destroy :check_for_bookmarks
+
+  private
+
+  def check_for_bookmarks
+    if bookmarks.any?
+      errors.add(:base, "Cannot delete movie with existing bookmarks")
+      throw(:abort)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#require 'open-uri'
+#require 'json'
+
+api_url = "https://tmdb.lewagon.com/movie/top_rated"
+
+data = JSON.load(URI.open(api_url))
+movies = data['results']
+
+movies.each do |movie|
+  Movie.create({ title: movie['title'],
+                 overview: movie['overview'],
+                 poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+                 rating: movie['vote_average']})
+end
