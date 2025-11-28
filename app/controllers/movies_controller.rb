9df7953303c99e6b274999ec64ebc15454828334
Lalon Sander <lalon.sander@gmail.com>
2025-11-28 18:51:12 +0100

    add movie pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: [:show]
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all.sort_by { |movie| -movie.bookmarks.count }
   end
 
   def show
